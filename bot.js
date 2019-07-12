11738f9a73b900889df2e7157ab5dad5d92515a6
laurensbutt <44417967+laurensbutt@users.noreply.github.com>
2019-07-11 22:25:28 -0400

    testing visuals

--- bot.js
+++ bot.js
@@ -3,8 +3,10 @@ const Discord = require('discord.js');
 const client = new Discord.Client();
 var deployed = new Boolean(false);
 var started = new Boolean(false);
+
 var rangers = [];
  
+const officerPic = new Attachment('img\officer.png');
 
 client.on('ready', () => {
 
@@ -17,21 +19,24 @@ client.on('ready', () => {
 client.on('message', message => {
 
     if (message.content === '!patrol') {
-        message.channel.send("Listen up, rangers. We need a team to get us intel on a recently discovered NVA compound. Who's in? (type **j** to join)");
+        message.channel.send(officerPic);
+        message.channel.send("Listen up, rangers. We need a team to get us intel on a recently discovered NVA compound. Who's in?");
+        message.channel.send("Enter :regional_indicator_j: to join")
         started = true;
     }
 
-    if (message.content === '!patrol' && started)
+    if (message.content === 'end patrol' && started)
     {
+        message.channel.send(officerPic);
         message.channel.send("Shows over folks!");
         started = false;
     }
 
     // If ranger joins team
-    if (message.content === 'j' && started) {
-        
+    if (message.content === 'j' && started || message.content === 'J' && started) {
+        message.channel.send(officerPic);
         rangers.push(message.author.tag);
-        message.channel.send(message.author.username + " has volunteered!");
+        message.channel.send(rangers[rangers.length - 1] + " has volunteered!");
     }
 
 });
