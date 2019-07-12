469ef8ebeb644bea13b0f86bf4ee7d8e8c66cff1
laurensbutt <44417967+laurensbutt@users.noreply.github.com>
2019-07-11 23:06:11 -0400

    visuals again, with Attachments

--- bot.js
+++ bot.js
@@ -1,16 +1,16 @@
 const Discord = require('discord.js');
-
+const Attachment = require('discord.js');
 const client = new Discord.Client();
 var deployed = new Boolean(false);
 var started = new Boolean(false);
 
 var rangers = [];
  
-const officerPic = new Attachment('img\officer.png');
 
 client.on('ready', () => {
 
     console.log('I am ready!');
+    const officerPic = new Attachment('img\officer.png');
 
 });
 
@@ -21,22 +21,21 @@ client.on('message', message => {
     if (message.content === '!patrol') {
         message.channel.send(officerPic);
         message.channel.send("Listen up, rangers. We need a team to get us intel on a recently discovered NVA compound. Who's in?");
-        message.channel.send("Enter :regional_indicator_j: to join")
+        message.channel.send("Enter :regional_indicator_j: to join");
         started = true;
     }
 
     if (message.content === 'end patrol' && started)
     {
-        message.channel.send(officerPic);
         message.channel.send("Shows over folks!");
         started = false;
     }
 
     // If ranger joins team
     if (message.content === 'j' && started || message.content === 'J' && started) {
-        message.channel.send(officerPic);
+        
         rangers.push(message.author.tag);
-        message.channel.send(rangers[rangers.length - 1] + " has volunteered!");
+        message.channel.send(message.author.username + " has volunteered!");
     }
 
 });
