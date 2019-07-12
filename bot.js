09081b074d38ca7ccfbcadbf2715f2af659a9d56
laurensbutt <44417967+laurensbutt@users.noreply.github.com>
2019-07-12 00:33:13 -0400

    ughh

--- bot.js
+++ bot.js
@@ -1,43 +1,26 @@
-require('dotenv').config();
+// Extract the required classes from the discord.js module
 const { Client, Attachment } = require('discord.js');
+
+// Create an instance of a Discord client
 const client = new Client();
-var deployed = new Boolean(false);
-var started = new Boolean(false);
-var rangers = [];
- 
 
+/**
+ * The ready event is vital, it means that only _after_ this will your bot start reacting to information
+ * received from Discord
+ */
 client.on('ready', () => {
-
-    console.log('I am ready!');
-
+  console.log('I am ready!');
 });
-
- 
 
 client.on('message', message => {
-    if (message.content === '!patrol') {
+    // If the message is '!rip'
+    if (message.content === '!rip') {
+        // Create the attachment using Attachment
         const attachment = new Attachment('https://i.imgur.com/w3duR07.png');
+        // Send the attachment in the message channel
         message.channel.send(attachment);
-        message.channel.send("Listen up, rangers. We need a team to get us intel on a recently discovered NVA compound. Who's in?");
-        message.channel.send("Enter :regional_indicator_j: to join");
-        started = true;
     }
-
-    if (message.content === 'end patrol' && started)
-    {
-        message.channel.send("Shows over folks!");
-        started = false;
-    }
-
-    // If ranger joins team
-    if (message.content === 'j' && started || message.content === 'J' && started) {
-        
-        rangers.push(message.author.tag);
-        message.channel.send(message.author.username + " has volunteered!");
-    }
-
 });
-
  
 
 // THIS  MUST  BE  THIS  WAY
